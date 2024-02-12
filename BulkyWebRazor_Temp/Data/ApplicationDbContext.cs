24c4a0d5b17fa790d50df716a69f80fd568ff77c
KrutarthZala <krutarthzala2@gmail.com>
2024-02-12 18:59:46 +0530

    Create New Project BulkyWebRazor_Temp.

---
+++ BulkyWebRazor_Temp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkyWebRazor_Temp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWebRazor_Temp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<CategoryModel> Categories { get; set; }
+    }
+}
