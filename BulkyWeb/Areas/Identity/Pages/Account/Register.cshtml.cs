da41117e141f6b2f0827a309772a243267fb9c9d
KrutarthZala <krutarthzala2@gmail.com>
2024-02-19 17:52:17 +0530

    Update Log in UI

--- BulkyWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BulkyWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -151,7 +151,7 @@ namespace BulkyBookWeb.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.BulkyBookUserName, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 user.BulkyBookUserName = Input.BulkyBookUserName;
                 user.UserStreetAddress = Input.UserStreetAddress;
