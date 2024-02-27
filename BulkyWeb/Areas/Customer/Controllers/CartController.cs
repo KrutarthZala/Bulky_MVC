e7ef5ee18f9b1cd18802ddc975560b1e60dfc0af
KrutarthZala <krutarthzala2@gmail.com>
2024-02-27 11:26:40 +0530

    Complete Shopping Cart Functionality.

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -38,6 +38,52 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
             return View(ShoppingCartVM);
         }
 
+        public IActionResult CartSummary()
+        {
+            return View();
+        }
+
+        #region Add Cart
+        public IActionResult AddCart(int cartID)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.CartID == cartID);
+            cartFromDb.ProductCount += 1;
+            _unitOfWork.ShoppingCart.Update(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+        #endregion
+
+        #region Subtract Cart
+        public IActionResult SubtractCart(int cartID)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.CartID == cartID);
+            if(cartFromDb.ProductCount <= 1)
+            {
+                // Remove from cart
+                _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            }
+            else
+            {
+                cartFromDb.ProductCount -= 1;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+            }
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+        #endregion
+
+        #region Remove Cart
+        public IActionResult DeleteCart(int cartID)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.CartID == cartID);
+            _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+        #endregion
+
+        #region Get Price Based on Quantity
         private double GetPriceBasedOnQuantity(ShoppingCartModel shoppingCart)
         {
             if(shoppingCart.ProductCount <= 50)
@@ -56,5 +102,6 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
                 }
             }
         }
+        #endregion
     }
 }
